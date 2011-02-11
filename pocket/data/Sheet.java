e2e671b1ce6f15e94885f11a52836865a9edce03
Pocket7878 <masa007>
2011-02-11 19:40:14 +0900

    Define some methods

--- pocket/data/Sheet.java
+++ pocket/data/Sheet.java
@@ -1,30 +1,38 @@
 package pocket.data.Sheet;
-import java.util.Vector;
 
 public class Sheet {
-	private Vector data;
+	private Object data[][];
 	private int width;
 	private int height;
 
 	public Sheet() {
 		this.width = 1;
 		this.height = 1;
-		this.data = new Vector(1);
+		this.data = new Object[this.width][this.height];
 	}
 
 	public Sheet(int w, int h) {
 		this.width = w;
 		this.height = h;
-		this.data = new Vector(this.width * this.height);
+		this.data = new Object[this.width][this.height];
 	}
 
 	public void resize(int newW, int newH) {
+		Object newData = new Object[newW][newH];
+		for(int i = 0; i < this.width && i < this.newW; i++) {
+			for(int j = 0; j < this.height && j < this.newW; j++) {
+				newData[i][j] = this.data[i][j];
+			}
+		}
+		this.data = newData;
 	}
 
 	public void setCell(int x, int y, Object data) {
+		this.data[x][y] = data;
 	}
 
 	public Object getCell(int x, int y) {
+		return this.data[x][y];
 	}
 
 	public void turnRight() {
@@ -43,11 +51,19 @@ public class Sheet {
 	}
 
 	public Object[] getRow(int x) {
+		Object returnData = new Object[this.height];
+		for(int i = 0; i < this.height; i++) {
+			returnData[i] = this.data[x][i];
+		}
 	}
 
 	public void setCol(int y, Object data[]) {
 	}
 
 	public Object[] getCol(int y) {
+		Object returnData = new Object[this.width];
+		for(int i = 0; i < this.width; i++) {
+			returnData[i] = this.data[i][y];
+		}
 	}
 }
